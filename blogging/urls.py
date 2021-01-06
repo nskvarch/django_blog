14c82139971caec8fc16af630eec4272b0a029bf
Niels Skvarch <nielsk01@noa.nintendo.com>
2021-01-06 13:32:55 -0800

    lesson 7 added list views and additional front end tests

--- blogging/urls.py
+++ blogging/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from blogging.views import stub_view
 
 urlpatterns = [
+    path('posts/<int:post_id>/', stub_view, name="blog_detail"),
     path('', stub_view, name="blog_index"),
 ]
