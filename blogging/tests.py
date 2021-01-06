e6d7aa1e0b247f75da15b09e6eca35b35b4314f6
Niels Skvarch <nielsk01@noa.nintendo.com>
2021-01-06 12:46:04 -0800

    fixed model.py from lesson 6 and started lesson 7 work

--- blogging/tests.py
+++ blogging/tests.py
@@ -3,9 +3,11 @@ from django.test import TestCase
 # Create your tests here.
 from django.contrib.auth.models import User
 from blogging.models import Post
+from blogging.models import Category
 
 
 class PostTestCase(TestCase):
+
     fixtures = ['blogging_test_fixture.json', ]
 
     def setUp(self):
@@ -15,4 +17,13 @@ class PostTestCase(TestCase):
         expected = "This is a title"
         p1 = Post(title=expected)
         actual = str(p1)
+        self.assertEqual(expected, actual)
+
+        
+class CategoryTestCase(TestCase):
+
+    def test_string_representation(self):
+        expected = "A Category"
+        c1 = Category(name=expected)
+        actual = str(c1)
         self.assertEqual(expected, actual)
