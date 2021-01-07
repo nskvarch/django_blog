729bbe5f5d96ebfe203c47658d1c3a2d8c19ec7a
Niels Skvarch <nielsk01@noa.nintendo.com>
2021-01-07 10:33:51 -0800

    lesson 7 assignment "fix category workflow for post editing"

--- blogging/admin.py
+++ blogging/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline,]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline,]
+    exclude = ('posts',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
